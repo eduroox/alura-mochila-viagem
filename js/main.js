5d860cdfef1dfdd5d3e51532fa2da552dd518644
Eduardo Brod <eduardo_brod@hotmail.com>
2023-05-10 19:21:01 -0300

    criação do arquivo JS

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach((elemento) => {
+  criarElemento(elemento);
+});
+
+//Validação form
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
+
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
+
+  //Criando lista de itens
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value,
+  };
+
+  if (existe) {
+    itemAtual.id = existe.id;
+    atualizaElemento(itemAtual);
+  } else {
+    itemAtual.id = itens.length;
+    criarElemento(itemAtual);
+    itens.push(itemAtual);
+  }
+
+  //Salvando elemento no localStorage
+  localStorage.setItem("itens", JSON.stringify(itens));
+
+  nome.value = "";
+  quantidade.value = "";
+});
+
+function criarElemento(item) {
+  //Criando um elemento
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
+
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
+  novoItem.appendChild(numeroItem);
+
+  novoItem.innerHTML += item.nome;
+
+  lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
